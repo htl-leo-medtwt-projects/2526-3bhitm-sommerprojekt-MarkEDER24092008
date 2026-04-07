14239fb3b94c80f44ed05ba11450ef7ba457821e
Mark <m.eder4@students.htl-leonding.ac.at>
2026-04-07 16:33:38 +0200

    password view toggle, fehlerbehebung in php

--- project/public/createUserMySql.php
+++ project/public/createUserMySql.php
@@ -1,6 +1,13 @@
 <?php 
+SESSION_START();
+
+$_db_host = "db_server";
+$_db_datenbank = "IndiGo";
+$_db_username = "super-root";
+$_db_passwort = "000";
+
 #open database connection
-$conn = new mysqli("_db_host", "_db_username", "_db_passwort", "_db_datenbank");
+$conn = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
 
 // check connection
 if ($conn->connect_error) {
@@ -10,19 +17,19 @@ if ($conn->connect_error) {
     if(!empty($_POST["submit"])) {
         $_username = $conn-> real_escape_string($_POST["username"]);
         $_password = $conn-> real_escape_string($_POST["password"]);
-        if(strcmp($_passwort, $conn->real_escape_string($_POST["password2"])) != 0) {
+        if(strcmp($_password, $conn->real_escape_string($_POST["password2"])) != 0) {
             #password is not repeated correctly
             include("create_user_form.html");
             exit;
         }
         #create password hash from original password
         #VARCHAR 60 necessary, but officially PHP reccomendation: at least 255 characters
-        $_passwortHash = password_hash($_passwort, PASSWORD_BCRYPT);
+        // $_passwortHash = password_hash($_passwort, PASSWORD_BCRYPT);
 
         #Statement for insert the values of the new user
 
-        $insertStatement = "INSERT INTO user (username,email, password, created_at, streak_count, last_login, xp, language_id) 
-                            VALUES ('$_username','', '$_passwortHash', NOW(),0 , NOW(), 0, 0);";   
+        $insertStatement = "INSERT INTO user (username, email, password, created_at, streak_count, last_login, xp, language_id) 
+                            VALUES ('$_username','', '$_password', NOW(),0 , NOW(), 0, 0);";   
 
         if($_res = $conn->query($insertStatement)) {
             echo "<br>USER $_username has been added to the database. <br> Try to log in.";
