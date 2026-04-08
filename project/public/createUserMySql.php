444fb3ade199a2b124ee98fa1472e679a95e396a
Mark <m.eder4@students.htl-leonding.ac.at>
2026-04-08 08:44:02 +0200

    fatal error in php gelöst, email muss gemacht werden

--- project/public/createUserMySql.php
+++ project/public/createUserMySql.php
@@ -17,19 +17,20 @@ if ($conn->connect_error) {
     if(!empty($_POST["submit"])) {
         $_username = $conn-> real_escape_string($_POST["username"]);
         $_password = $conn-> real_escape_string($_POST["password"]);
-        if(strcmp($_password, $conn->real_escape_string($_POST["password2"])) != 0) {
-            #password is not repeated correctly
-            include("create_user_form.html");
-            exit;
-        }
+        $_email = htmlspecialchars($conn-> real_escape_string($_POST["email"]));
+        // if(strcmp($_password, $conn->real_escape_string($_POST["password2"])) != 0) {
+        //     #password is not repeated correctly
+        //     include("create_user_form.html");
+        //     exit;
+        // }
         #create password hash from original password
         #VARCHAR 60 necessary, but officially PHP reccomendation: at least 255 characters
         // $_passwortHash = password_hash($_passwort, PASSWORD_BCRYPT);
 
         #Statement for insert the values of the new user
 
-        $insertStatement = "INSERT INTO user (username, email, password, created_at, streak_count, last_login, xp, language_id) 
-                            VALUES ('$_username','', '$_password', NOW(),0 , NOW(), 0, 0);";   
+        $insertStatement = "INSERT INTO user (username, email, password, created_at, streak_count, last_login, xp) 
+                            VALUES ('$_username',$_email, '$_password', NOW(), 0, NOW(), 0);";   
 
         if($_res = $conn->query($insertStatement)) {
             echo "<br>USER $_username has been added to the database. <br> Try to log in.";
