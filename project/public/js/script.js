3de2ced5ea513567791c4d3c1e7c98e5200824da
Mark <m.eder4@students.htl-leonding.ac.at>
2026-04-21 16:10:38 +0200

    sehr viel arbeit

--- project/public/js/script.js
+++ project/public/js/script.js
@@ -1,8 +1,13 @@
 function togglePasswordVisibility() {
-  var x = document.getElementById("passwordInput");
+  let x = document.getElementById("passwordInput");
+  let x2 = document.getElementById("eye");
   if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
+  x.type = "text";
+  x2.src = "./media/images/privacyViewing.png";
+  x2.alt = "Hide Password";
+} else {
+  x.type = "password";
+  x2.src = "./media/images/privacyHiding.png";
+  x2.alt = "Show Password";
+}
 }
